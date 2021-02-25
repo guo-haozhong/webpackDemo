916d97c0d76d7b23a804f07f838a7e8634351d97
Writing the same code as poetry <31845115+fengandrain@users.noreply.github.com>
2021-02-25 10:25:00 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   devtool: 'eval-source-map',
-  entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
+  entry: __dirname + "/app/main.js",//唯一入口文件
   output: {
     path: __dirname + "/build",//打包后的文件存放的地方
     filename: "bundle.js"//打包后输出文件的文件名
